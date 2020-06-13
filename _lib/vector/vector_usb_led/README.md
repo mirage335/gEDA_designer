ef10cd5409587e3b3f58f673b2d606733c710b16
mirage335 <spamfreemirage335@gmail.com>
2020-06-13 05:42:40 -0400

    Simple vector test PCB.

---
+++ _lib/vector/vector_usb_led/README.md
@@ -0,0 +1,11 @@
+Simple USB powered LED circuit. Mostly intended to provide vector test for gEDA_designer . Some effort has been made to demonstrate best practices and include fabrication test cases.
+
+May be manufactured as dual-layer, quad-layer, or six-layer.
+
+
+# Future Work
+
+* Via sizes could be changed to conform to 34mil hole size, 8mil annular ring size, 50mil outside diameter, and/or 25mil milldrill minimum hole size. This would improve the usefulness of this example for less than ideal photolithography manufacturing.
+
+* PCB-INTEGRAL Trace based connector... could be marked noplace/nobom or similar.
+
---
+++ _lib/vector/vector_usb_led/usb_led.cmd
@@ -0,0 +1,48 @@
+# Pin name action command file
+
+# Start of element LED1
+ChangePinName(LED1, 2, 2)
+ChangePinName(LED1, 1, 1)
+
+# Start of element T1
+ChangePinName(T1, 2, 2)
+ChangePinName(T1, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element LED2
+ChangePinName(LED2, 2, 2)
+ChangePinName(LED2, 1, 1)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element T2
+ChangePinName(T2, 2, 2)
+ChangePinName(T2, 1, 1)
+
+# Start of element FID2
+
+# Start of element FID1
+
+# Start of element FID3
+
+# Start of element FID4
+
+# Start of element USB
+ChangePinName(USB, 5, 5)
+ChangePinName(USB, 3, 3)
+ChangePinName(USB, 1, 1)
+ChangePinName(USB, 4, 4)
+ChangePinName(USB, 2, 2)
